fdf824eb9e8f4c2f538d349df66f9e61aa635ba7
jm <jmleeluck@snu.ac.kr>
2024-11-18 21:53:48 +0900

    update prompts

---
+++ prompts.py
@@ -0,0 +1,4 @@
+PROMPT_VER1 = """
+        This track represents ordinary media content situation that includes the dialogue with background musics.
+        You should separate this into dialogue, lyrics, background musics, and noise.
+    """
