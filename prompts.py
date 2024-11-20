13bd59c1f7dfba10910e66e84a26b60f9cd7ccd7
jm <jmleeluck@snu.ac.kr>
2024-11-20 12:27:06 +0900

    update prompts

--- prompts.py
+++ prompts.py
@@ -2,3 +2,7 @@ PROMPT_VER1 = """
         This track represents ordinary media content situation that includes the dialogue with background musics.
         You should separate this into dialogue, lyrics, background musics, and noise.
     """
+
+PROMPT_VER2 = """
+        Separate the track into dialogue, lyrics, background musics, and noise.
+    """
