af7cfe67b2945396494a082686f5985d0b4bed05
jm <jmleeluck@snu.ac.kr>
2024-11-20 16:16:58 +0900

    update eval

--- eval.py
+++ eval.py
@@ -8,6 +8,31 @@ import argparse
 from models.dataset import MediaContentDataset
 from tqdm import tqdm
 from prompts import *
+from utils import *
+import matplotlib.pyplot as plt
+from torchlibrosa.stft import Spectrogram
+
+
+class SpectralL1Loss(nn.Module):
+    def __init__(self, n_fft = 512, hop_size = 256, win_size = 512):
+        super().__init__()
+        self.n_fft = n_fft
+        self.hop_length = hop_size
+        self.win_length = win_size
+        self.spectrogram_extractor = Spectrogram(n_fft=self.n_fft, hop_length=self.hop_length, 
+            win_length=self.win_length, window="hann", center=True, pad_mode="reflect", 
+            freeze_parameters=True).to(train_args.device)
+        
+        self.l1_loss = nn.L1Loss()
+
+    def forward(self, predicted, target):
+        loss = 0
+        for stem in range(4):
+            pred_spectrogram = self.spectrogram_extractor(predicted[:, stem, :])
+            target_spectrogram = self.spectrogram_extractor(target[:, stem, :])
+
+            loss += self.l1_loss(pred_spectrogram, target_spectrogram)
+        return loss/4
 
 class MSSEvaluator:
     def __init__(self, model, preprocess_text, prompts, eval_args):
@@ -19,8 +44,6 @@ class MSSEvaluator:
         self.duration = eval_args.dataset_config["duration"]
         self.preprocess_text = preprocess_text
         self.prompts = prompts
-
-        self.loss_fn = nn.L1Loss()
 
         csv_file_path = os.path.join(self.data_dir, "processed_data", "test.csv")
         self.dataset = MediaContentDataset(
@@ -36,12 +59,11 @@ class MSSEvaluator:
             shuffle=False,
         )
 
-    def evaluate(self, checkpoint_path):
+        self.loss_fn = SpectralL1Loss(eval_args.n_fft, eval_args.hop_size, eval_args.window_size)
+
+
+    def evaluate(self):
         self.model.eval()
-        print(f"Starting evaluation using checkpoint: {checkpoint_path}")
-
-        self.model.load_state_dict(torch.load(checkpoint_path, map_location=self.device))
-
         eval_losses = []
         bar = tqdm(total=len(self.eval_dl), desc="Evaluating")
 
@@ -49,79 +71,45 @@ class MSSEvaluator:
             for batch_idx, (inputs, labels) in enumerate(self.eval_dl):
                 inputs = inputs.to(self.device)
                 labels = [label.to(self.device) for label in labels]
+                labels = torch.stack(labels, dim=1)
 
                 text_encs = self.preprocess_text(self.prompts * len(inputs), enc_tok=True, add_text=False)
 
                 outputs = self.model(inputs, text_encs)
 
-                loss = self.loss_fn(outputs, torch.stack(labels, dim=1))
+                loss = self.loss_fn(outputs, labels)
                 eval_losses.append(loss.item())
 
                 bar.update(1)
-                bar.set_description(f"Eval Loss: {sum(eval_losses) / len(eval_losses):.5f}")
+                bar.set_description(f"[INFO] Eval Loss: {sum(eval_losses) / len(eval_losses):.5f}")
+                
+                if batch_idx ==0 :
+                    plt.figure()
+                    plt.plot(outputs[batch_idx, 0, :].cpu().detach().numpy(), label = "prediected")
+                    plt.plot(labels[batch_idx, 0].cpu().detach().numpy(), label = "ground truth")
+                    plt.legend()
+                    plt.savefig("./output.png")
 
         avg_loss = sum(eval_losses) / len(eval_losses)
-        print(f"\nEvaluation completed with average loss: {avg_loss:.5f}")
+        print(f"\n[INFO] Evaluation completed with average loss: {avg_loss:.5f}")
         return avg_loss
 
 if __name__ == "__main__":
     os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
-    def read_config_as_args(config_path):
-        return_dict = {}
-        with open(config_path, "r") as f:
-            yml_config = yaml.load(f, Loader=yaml.FullLoader)
-        for k, v in yml_config.items():
-            return_dict[k] = v
-        return argparse.Namespace(**return_dict)
-    
+    eval_config_path = "configs/demo.yml"
+    base_config_path = "configs/base_mss.yml"
 
-    def get_latest_checkpoint(model_name):
-        base_path = f"./checkpoint/{model_name}"
-
-        if not os.path.exists(base_path):
-            raise FileNotFoundError(f"No checkpoint directory found for model: {model_name}")
-
-        checkpoint_dirs = sorted(
-            [d for d in os.listdir(base_path) if os.path.isdir(os.path.join(base_path, d))],
-            reverse=True
-        )
-
-        if not checkpoint_dirs:
-            raise FileNotFoundError("No checkpoint directories found.")
-
-        latest_checkpoint_dir = os.path.join(base_path, checkpoint_dirs[0])
-
-        checkpoint_files = sorted(
-            [f for f in os.listdir(latest_checkpoint_dir) if f.startswith("model-epoch-")],
-            key=lambda x: int(x.split("-epoch-")[1].split(".")[0]),
-            reverse=True
-        )
-
-        if not checkpoint_files:
-            raise FileNotFoundError("No checkpoint files found in the latest directory.")
-
-        latest_checkpoint_path = os.path.join(latest_checkpoint_dir, checkpoint_files[0])
-
-        return latest_checkpoint_path
-
-
-    train_config_path = "configs/train.yml"
-    base_config_path = "configs/base.yml"
-
-    train_args = read_config_as_args(train_config_path)
+    train_args = read_config_as_args(eval_config_path)
     base_args = read_config_as_args(base_config_path)
     merged_dicts = {**vars(base_args), **vars(train_args)}
     args = argparse.Namespace(**merged_dicts)
 
-    mss_wrapper = MSSWrapper(config="base", use_cuda=True)
+    mss_wrapper = MSSWrapper(config="eval", use_cuda=True)
     model, tokenizer = mss_wrapper.model, mss_wrapper.enc_tokenizer
 
-    prompts = [PROMPT_VER1]
+    prompts = [PROMPT_VER2]
 
     evaluator = MSSEvaluator(model, preprocess_text=mss_wrapper.preprocess_text, prompts=prompts, eval_args=args)
 
-    checkpoint_path = get_latest_checkpoint(train_args.model_name)
-    print(f"Loading checkpoint from: {checkpoint_path}")
-
-    evaluator.evaluate(checkpoint_path)
+    evaluator.evaluate()
